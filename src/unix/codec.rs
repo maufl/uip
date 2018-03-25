812e23d811694879dc225f8dac229d6213451de9
Felix Konstantin Maurer <maufl@maufl.de>
2018-03-25 22:47:53 +0200

    Move to msgpack for unix socket protocol

--- src/unix/codec.rs
+++ src/unix/codec.rs
@@ -1,14 +1,11 @@
 use std::io::{Error, ErrorKind, Result};
-use serde::{Deserialize, Serialize};
-use rmp_serde::{Deserializer, Serializer};
-use rmp_serde::encode::Error as EncodeError;
-use byteorder::{BigEndian, ByteOrder};
+use serde::Serialize;
+use rmp_serde::{from_slice, Serializer};
 use bytes::BytesMut;
-use bytes::buf::BufMut;
 use tokio_io::codec::{Decoder, Encoder};
 use Identifier;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Frame {
     Connect(Identifier, u16),
     Data(Vec<u8>),
@@ -23,15 +20,12 @@ impl Decoder for ControlProtocolCodec {
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Frame>> {
         debug!("Decoding UNIX message: {:?}", buf);
-        if buf.len() < 3 {
-            return Ok(None);
-        }
-        let typ = buf.as_ref()[0];
-        match typ {
-            1u8 => parse_connect(buf),
-            2u8 => parse_data(buf),
-            _ => Err(Error::new(ErrorKind::Other, "Invalid packet")),
-        }
+        from_slice(&buf).map(|frame| Some(frame)).map_err(|err| {
+            Error::new(
+                ErrorKind::Other,
+                format!("Error while decoding message: {}", err),
+            )
+        })
     }
 }
 
@@ -40,47 +34,14 @@ impl Encoder for ControlProtocolCodec {
     type Error = Error;
 
     fn encode(&mut self, msg: Frame, buf: &mut BytesMut) -> Result<()> {
-        match msg {
-            Frame::Connect(host_id, channel_id) => encode_connect(&host_id, channel_id, buf),
-            Frame::Data(data) => encode_data(&data, buf),
+        let mut tmp = Vec::new();
+        if let Err(err) = msg.serialize(&mut Serializer::new(&mut tmp)) {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("Error while serializing frame: {}", err),
+            ));
         }
+        buf.extend_from_slice(&tmp);
+        Ok(())
     }
-}
-
-fn encode_connect(host_id: &Identifier, channel_id: u16, buf: &mut BytesMut) -> Result<()> {
-    buf.reserve(5 + host_id.len());
-    buf.put_u8(1);
-    buf.put_u16::<BigEndian>(host_id.len() as u16);
-    buf.put_slice(host_id.as_bytes());
-    buf.put_u16::<BigEndian>(channel_id);
-    Ok(())
-}
-
-fn encode_data(data: &Vec<u8>, buf: &mut BytesMut) -> Result<()> {
-    buf.reserve(3 + data.len());
-    buf.put_u8(2);
-    buf.put_u16::<BigEndian>(data.len() as u16);
-    buf.put_slice(&data);
-    Ok(())
-}
-
-fn parse_connect(buf: &mut BytesMut) -> Result<Option<Frame>> {
-    let len = BigEndian::read_u16(&buf.as_ref()[1..3]) as usize;
-    if buf.len() < len + 5 {
-        return Ok(None);
-    };
-    let host_id = Identifier::copy_from_slice(&buf.as_ref()[3..len + 3])
-        .map_err(|_| Error::new(ErrorKind::Other, "Invalid host identifier"))?;
-    let channel_id = BigEndian::read_u16(&buf.as_ref()[len + 3..len + 5]);
-    let _ = buf.split_to(len + 5);
-    Ok(Some(Frame::Connect(host_id, channel_id)))
-}
-
-fn parse_data(buf: &mut BytesMut) -> Result<Option<Frame>> {
-    let len: usize = BigEndian::read_u16(&buf.as_ref()[1..3]) as usize;
-    if buf.len() < len + 3 {
-        return Ok(None);
-    };
-    let _ = buf.split_to(3);
-    Ok(Some(Frame::Data(buf.split_to(len).as_ref().to_vec())))
 }
