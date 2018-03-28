f51cf814630fda6ddad33641ad5d1a04bd094204
Felix Konstantin Maurer <maufl@maufl.de>
2018-03-28 21:51:33 +0200

    Set correct log level

--- src/bin/uipd.rs
+++ src/bin/uipd.rs
@@ -23,6 +23,15 @@ use clap::{App, Arg, SubCommand};
 
 fn main() {
     fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}] {}",
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Error)
         .level_for("uip", log::LevelFilter::Debug)
         .chain(std::io::stdout())
         .apply()
